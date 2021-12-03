b4e57f167817a69ca541c61c69a76ebbf3719008
duongminhthanh <duongminhthanh1999td@gmail.com>
2021-12-03 16:26:28 +0700

    make it better

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 require('dotenv').config()
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
 
+const authRouter = require('./routes/auth')
 const postRouter = require('./routes/post')
 
-const authRouter = require('./routes/auth')
+const connectDB = async () => {
+	try {
+		await mongoose.connect(
+			`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-learnit.j0fcc.mongodb.net/mern-learnit?retryWrites=true&w=majority`,
+			{
+				useCreateIndex: true,
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useFindAndModify: false
+			}
+		)
 
-const PORT = process.env.PORT || 5002;
+		console.log('MongoDB connected')
+	} catch (error) {
+		console.log(error.message)
+		process.exit(1)
+	}
+}
+
+connectDB()
+
+const app = express()
 app.use(express.json())
 app.use(cors())
 
 app.use('/api/auth', authRouter)
 app.use('/api/posts', postRouter)
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lzqrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority1`).then(() => {
-    app.listen(PORT, () => console.log(`Server started on ${PORT}`));
-}).catch(err => {
-    console.log(err)
-})
+const PORT = process.env.PORT || 5000
 
-
-
-
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
